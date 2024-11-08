13bad51d757367d93755a30e843c00692fa3a940
Rinoy Francis <rinoyfrancis@Rinoys-MacBook-Air-2.local>
2024-11-08 01:13:15 +0000

    First commit

---
+++ PhishImmune/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PhishImmune
+//
+//  Created by Rinoy Francis on 08/11/2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
